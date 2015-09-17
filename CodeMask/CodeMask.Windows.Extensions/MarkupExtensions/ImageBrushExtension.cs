d9ca70a808f24afbf33ebb3f330c6fb0b72d782e
cooglex <cooglex@qq.com>
2015-09-17 15:32:47 +0800

    添加xaml标记扩展类ImageBrushExtension&ImageSourceExtension

---
+++ CodeMask/CodeMask.Windows.Extensions/MarkupExtensions/ImageBrushExtension.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace CodeMask.Windows.Extensions.MarkupExtensions
+{
+    /// <summary>
+    ///     ImageBrush标记扩展。
+    /// </summary>
+    /// <example>
+    ///     <code>
+    ///          <![CDATA[
+    ///          ]]>
+    ///     </code>
+    /// </example>
+    public class ImageBrushExtension : MarkupExtension
+    {
+        #region 私有变量
+
+        /// <summary>
+        ///     图片相对路径。
+        /// </summary>
+        private readonly object _relativePath;
+
+        #endregion
+
+        #region 构造
+
+        /// <summary>
+        ///     构造。
+        /// </summary>
+        /// <param name="relativePath">图片相对路径。</param>
+        public ImageBrushExtension(object relativePath)
+        {
+            _relativePath = relativePath;
+        }
+
+        #endregion
+
+        #region 重写方法
+
+        /// <summary>
+        ///     返回值。
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (_relativePath.GetType() == typeof (ImageSource))
+                return new ImageBrush((ImageSource) _relativePath);
+            return
+                new ImageBrush(
+                    (ImageSource)
+                        new ImageSourceConverter().ConvertFromString("pack://siteOfOrigin:,,,/" + _relativePath));
+        }
+
+        #endregion
+    }
+}
---
+++ CodeMask/CodeMask.Windows.Extensions/MarkupExtensions/ImageSourceExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace CodeMask.Windows.Extensions.MarkupExtensions
+{
+    /// <summary>
+    ///     ImageSource 标记扩展类。
+    /// </summary>
+    /// <example>
+    ///     <code>
+    ///          <![CDATA[
+    ///          ]]>
+    ///     </code>
+    /// </example>
+    public class ImageSourceExtension : MarkupExtension
+    {
+        #region 私有变量
+
+        private readonly ImageSource _source;
+
+        #endregion
+
+        #region 构造
+
+        /// <summary>
+        ///     构造。
+        /// </summary>
+        /// <param name="relativePath">图片相对路径。</param>
+        public ImageSourceExtension(string relativePath)
+        {
+            _source =
+                (ImageSource) new ImageSourceConverter().ConvertFromString("pack://siteOfOrigin:,,,/" + relativePath);
+        }
+
+        #endregion
+
+        #region 重写方法
+
+        /// <summary>
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _source;
+        }
+
+        #endregion
+    }
+}
