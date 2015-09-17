a211f0d9b247dc017aaad71b8f109ee606192b87
cooglex <cooglex@qq.com>
2015-09-18 00:14:40 +0800

    添加IEnumerable.ToObservableCollection

---
+++ CodeMask/CodeMask.Windows.Extensions/System.Collections.Generic.IEnumerable[T]/IEnumerable.ToObservableCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CodeMask.Windows.Extensions_
+{
+    /// <summary>
+    ///     <see cref="System.Collections.Generic.IEnumerable&lt;T&gt;" />扩展类。
+    /// </summary>
+    public static partial class IEnumerableExtension
+    {
+        /// <summary>
+        ///     将<see cref="System.Collections.Generic.IEnumerable&lt;T&gt;" />集合转换成
+        ///     <see cref="System.Collections.ObjectModel.ObservableCollection&lt;T&gt;" />。
+        /// </summary>
+        /// <typeparam name="T">泛型类型参数。</typeparam>
+        /// <param name="this"><see cref="System.Collections.Generic.IEnumerable&lt;T&gt;" />集合对象。</param>
+        /// <example>
+        ///     <code>
+        ///          <![CDATA[
+        ///          ]]>
+        ///     </code>
+        /// </example>
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> @this)
+        {
+            var dest = new ObservableCollection<T>();
+
+            foreach (var item in @this)
+                dest.Add(item);
+
+            return dest;
+        }
+    }
+}
