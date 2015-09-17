7b7222f84907b6612ab617ae9181578938e25323
cooglex <cooglex@qq.com>
2015-09-18 00:54:35 +0800

    删除工程CodeMask.Windows

--- CodeMask/CodeMask.Windows/Services/ServiceProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodeMask.Windows.Services
-{
-    /// <summary>
-    ///     提供服务委托。
-    /// </summary>
-    /// <param name="context">服务对象。</param>
-    /// <returns>服务对象。</returns>
-    public delegate object ServiceProvider(object context = null);
-}
