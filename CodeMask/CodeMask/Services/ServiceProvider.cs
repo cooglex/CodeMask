a6cbab695bddae380c318b74c56d98852cbb6d50
cooglex <cooglex@qq.com>
2015-09-18 00:21:13 +0800

    添加CodeMask工程

---
+++ CodeMask/CodeMask/Services/ServiceProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeMask.Services
+{
+    /// <summary>
+    ///     提供服务委托。
+    /// </summary>
+    /// <param name="context">服务对象。</param>
+    /// <returns>服务对象。</returns>
+    public delegate object ServiceProvider(object context = null);
+}
