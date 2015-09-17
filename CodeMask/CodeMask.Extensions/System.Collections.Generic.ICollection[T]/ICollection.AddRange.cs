1a37f3c51b11e754624f9a65d70f543220918a0f
cooglex <cooglex@qq.com>
2015-09-18 00:00:38 +0800

    添加扩展方法ICollection.AddRange

---
+++ CodeMask/CodeMask.Extensions/System.Collections.Generic.ICollection[T]/ICollection.AddRange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace CodeMask.Extensions
+{
+    /// <summary>
+    ///     <see cref="System.Collections.Generic.ICollection&lt;T&gt;" />扩展类。
+    /// </summary>
+    public static class ICollectionExtension
+    {
+        /// <summary>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="input"></param>
+        /// <example>
+        ///     <code>
+        ///            <![CDATA[
+        ///            ]]>
+        ///       </code>
+        /// </example>
+        public static void AddRange<T>(this ICollection<T> @this, IEnumerable<T> input)
+        {
+            foreach (var item in input)
+                @this.Add(item);
+        }
+    }
+}
