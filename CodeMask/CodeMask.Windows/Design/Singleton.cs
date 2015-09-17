ef044ef3c0d7f3f202284be1eac1c4a08bd2dbf3
cooglex <cooglex@qq.com>
2015-09-17 09:42:57 +0800

    添加单例模式类Singleton

---
+++ CodeMask/CodeMask.Windows/Design/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CodeMask.Windows.Design
+{
+    /// <summary>
+    ///     单例模式类。
+    /// </summary>
+    /// <typeparam name="T">类型参数。</typeparam>
+    public class Singleton<T> where T : class, new()
+    {
+        /// <summary>
+        ///     实例对象。
+        /// </summary>
+        private static Lazy<T> _instance;
+
+        /// <summary>
+        ///     访问锁。
+        /// </summary>
+        private static readonly object syslock = new object();
+
+        /// <summary>
+        ///     获取相应类型的实例对象。
+        /// </summary>
+        /// <value>实例对象。</value>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syslock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Lazy<T>(true);
+                        }
+                    }
+                }
+                return _instance.Value;
+            }
+        }
+    }
+}
