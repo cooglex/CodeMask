e3d85dadfacab1a19474ac7429f06cd66100e147
cooglex <cooglex@qq.com>
2015-09-28 14:09:43 +0800

    添加IsNullConverter

---
+++ CodeMask/CodeMask.WPF/Converters/IsNullConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CodeMask.WPF.Converters
+{
+    /// <summary>
+    ///     判断是否为空转换器。
+    /// </summary>
+    public class IsNullConverter : CoreConverter<IsNullConverter>, IValueConverter
+    {
+        #region IValueConverter Members
+        /// <summary>
+        ///     转换值。
+        /// </summary>
+        /// <param name="value">绑定源生成的值。</param>
+        /// <param name="targetType">绑定目标属性的类型。</param>
+        /// <param name="parameter">要使用的转换器参数。</param>
+        /// <param name="culture">要用在转换器中的区域性。</param>
+        /// <returns>转换后的值。如果该方法返回 null，则使用有效的 null 值。</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null;
+        }
+
+        /// <summary>
+        ///     转换值。
+        /// </summary>
+        /// <param name="value">绑定目标生成的值。</param>
+        /// <param name="targetType">要转换到的类型。</param>
+        /// <param name="parameter">要使用的转换器参数。</param>
+        /// <param name="culture">要用在转换器中的区域性。</param>
+        /// <returns>转换后的值。如果该方法返回 null，则使用有效的 null 值。</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        } 
+        #endregion
+    }
+}
