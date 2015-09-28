274fb0bce44bf63d1c6344f4e684fa10eac3703b
cooglex <cooglex@qq.com>
2015-09-28 15:03:48 +0800

    添加CodeMask.UWP工程

---
+++ CodeMask/CodeMask.UWP/Converters/LowerConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace CodeMask.UWP.Converters
+{
+    /// <summary>
+    ///     小写转换器。
+    /// </summary>
+    public sealed class LowerConverter : CoreConverter<LowerConverter>, IValueConverter
+    {
+        #region IValueConverter Members
+
+        /// <summary>
+        ///     转换值。
+        /// </summary>
+        /// <param name="value">绑定源生成的值。</param>
+        /// <param name="targetType">绑定目标属性的类型。</param>
+        /// <param name="parameter">要使用的转换器参数。</param>
+        /// <param name="language">要用在转换器中的区域性。</param>
+        /// <returns>转换后的值。如果该方法返回 null，则使用有效的 null 值。</returns>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null)
+                return value.ToString().ToLower();
+            return value;
+        }
+
+        /// <summary>
+        ///     转换值。
+        /// </summary>
+        /// <param name="value">绑定目标生成的值。</param>
+        /// <param name="targetType">要转换到的类型。</param>
+        /// <param name="parameter">要使用的转换器参数。</param>
+        /// <param name="language">要用在转换器中的区域性。</param>
+        /// <returns>转换后的值。如果该方法返回 null，则使用有效的 null 值。</returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
