195f3b32c2640d1d3c09f765ecbd5e4fc18b4283
cooglex <cooglex@qq.com>
2016-08-09 17:28:29 +0800

    增加资源绑定demo

---
+++ CodeMask/CodeMask.WPF.Controls/Attached/AttachContentControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeMask.WPF.Controls.Attached
+{
+    public class AttachContentControl
+    {
+        #region ContentKey
+
+        public static DependencyProperty ContentKeyProperty = DependencyProperty.RegisterAttached("ContentKey",
+            typeof (string), typeof (AttachContentControl),
+            new PropertyMetadata(null, ContentKeyPropertyChangedCallback));
+
+        public static string GetContentKey(DependencyObject dependencyObject)
+        {
+            return (string) dependencyObject.GetValue(ContentKeyProperty);
+        }
+
+        public static void SetContentKey(DependencyObject dependencyObject, string value)
+        {
+            dependencyObject.SetValue(ContentKeyProperty, value);
+        }
+
+        private static void ContentKeyPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                var control = (d as ContentControl);
+                control.SetResourceReference(ContentControl.ContentProperty, e.NewValue);
+            }
+        }
+
+        #endregion
+
+        #region TagKey
+
+        public static DependencyProperty TagKeyProperty = DependencyProperty.RegisterAttached("TagKey",
+            typeof (string), typeof (AttachContentControl), new PropertyMetadata(null, TagKeyPropertyChangedCallback));
+
+        public static string GetTagKey(DependencyObject dependencyObject)
+        {
+            return (string) dependencyObject.GetValue(TagKeyProperty);
+        }
+
+        public static void SetTagKey(DependencyObject dependencyObject, string value)
+        {
+            dependencyObject.SetValue(TagKeyProperty, value);
+        }
+
+        private static void TagKeyPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                var control = (d as ContentControl);
+                control.SetResourceReference(FrameworkElement.TagProperty, e.NewValue);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ CodeMask/ThemeTest/resourceBindingDemo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ThemeTest
+{
+    /// <summary>
+    ///     resourceBindingDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class resourceBindingDemo : UserControl
+    {
+        private readonly ObservableCollection<string> Buttons = new ObservableCollection<string>();
+
+        public resourceBindingDemo()
+        {
+            InitializeComponent();
+            Loaded += ResourceBindingDemo_Loaded;
+        }
+
+        private void ResourceBindingDemo_Loaded(object sender, RoutedEventArgs e)
+        {
+            Buttons.Add("TextBlockKey");
+            //Buttons.Add("TextBlockKey");
+            ic.ItemsSource = Buttons;
+        }
+    }
+}
